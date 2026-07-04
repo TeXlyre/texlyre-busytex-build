b1b22ff12123b5f19bf0402656b2c8eac61873f5
Fares Abawi <f.abawi@outlook.com>
2026-07-04 11:35:51 +0100

    Enabled runtime shell handlers

--- kpse_remote.c
+++ kpse_remote.c
@@ -5,6 +5,7 @@
 
 #ifdef __EMSCRIPTEN__
 extern char *kpse_remote_fetch_js(const char *name, int format);
+extern int shell_escape_dispatch_js(const char *cmd, const char *cwd);
 #endif
 
 extern char *__real_kpse_find_file(const char *name, int format, int must_exist);
@@ -23,3 +24,18 @@ char *__wrap_kpse_find_file(const char *name, int format, int must_exist)
 
     return NULL;
 }
+
+#ifdef __EMSCRIPTEN__
+int system(const char *cmd)
+{
+    char cwd[4096];
+
+    if (cmd == NULL)
+        return 0;
+
+    if (getcwd(cwd, sizeof(cwd)) == NULL)
+        cwd[0] = '\0';
+
+    return shell_escape_dispatch_js(cmd, cwd);
+}
+#endif
